496336c827085de28a9b7c76cd70edc8caca8864
Elliot Greenlee <egreenle@vols.utk.edu>
2017-10-29 04:07:37 -0400

    no

--- app/model.py
+++ app/model.py
@@ -3,30 +3,56 @@ import subprocess
 
 
 def model(object_frames, output_clean):
-    print("Running SfM_GlobalPipeline.py")
+
+    print("Running openMVG_main_SfMInit_ImageListing")
     output_dir = "/app/pipe_out"
-    os.chdir("/app/openMVG_Build/software/SfM")
-    p = subprocess.Popen(("python", "SfM_SequentialPipeline.py", object_frames, output_dir))
+    os.chdir("/app/openMVG_Build/Linux-x86_64-RELEASE")  # 904.5?
+    p = subprocess.Popen(("./openMVG_main_SfMInit_ImageListing", "-i", object_frames, "-o", output_dir, "-f", "904", "-d", "/app/openMVG/src/openMVG/exif/sensor_width_database/sensor_width_camera_database.txt"))
+    p.wait()
+    os.chdir("/app")
+
+    print("Running openMVG_main_ComputeFeatures")
+    input_json = "/app/pipe_out/sfm_data.json"
+    output_dir = "/app/pipe_out/features"
+    os.chdir("/app/openMVG_Build/Linux-x86_64-RELEASE")
+    p = subprocess.Popen(("./openMVG_main_ComputeFeatures", "-i", input_json, "-o", output_dir))
+    p.wait()
+    os.chdir("/app")
+
+    print("Running openMVG_main_ComputeMatches")
+    input_json = "/app/pipe_out/sfm_data.json"
+    output_matches = "/app/pipe_out/matches"
+    os.chdir("/app/openMVG_Build/Linux-x86_64-RELEASE")
+    p = subprocess.Popen(("./openMVG_main_ComputeMatches", "-i", input_json, "-o", output_matches))  # "-v", "1"
+    p.wait()
+    os.chdir("/app")
+
+    print("Running openMVG_main_IncrementalSfM")
+    input_json = "/app/pipe_out/sfm_data.json"
+    input_matches = "/app/pipe_out/matches"
+    output_reconstruction = "/app/pipe_out/sequential_reconstruction"
+    os.chdir("/app/openMVG_Build/Linux-x86_64-RELEASE")
+    p = subprocess.Popen(("./openMVG_main_IncrementalSfM", "-i", input_json, "-m", input_matches, "-o", output_reconstruction))
     p.wait()
     os.chdir("/app")
 
     print("Running openMVG_main_openMVG2MVE2")
-    input_json = "/app/pipe_out/reconstruction_sequential/sfm_data.bin"
-    output_dir = "/app/pipe_out/matches"
+    input_bin = "/app/pipe_out/sequential_reconstruction/sfm_data.bin"
+    output_reconstruction = "/app/pipe_out/output_reconstruction"
     os.chdir("/app/openMVG_Build/Linux-x86_64-RELEASE")
-    p = subprocess.Popen(("./openMVG_main_openMVG2MVE2", "-i", input_json, "-o", output_dir))
-    p.wait()
-    os.chdir("/app")
-
-    print("Running dmrecon")
-    input_dir = "/app/pipe_out/matches/MVE"
-    os.chdir("/app/mve/apps/dmrecon")
-    # os.system("./dmrecon -s2 {}".format(input_dir))
-    p = subprocess.Popen(("./dmrecon", "-s2", input_dir))
+    p = subprocess.Popen(("./openMVG_main_openMVG2MVE2", "-i", input_bin, "-o", output_reconstruction))
     p.wait()
     os.chdir("/app")
 
     return "hi"
+
+    print("Running dmrecon")
+    input_dir = "/app/pipe_out/matches/MVE"
+    os.chdir("/app/mve/apps/dmrecon")
+    # os.system("./dmrecon -s2 {}".format(input_dir)) < this may need ot be doing
+    p = subprocess.Popen(("./dmrecon", "-s2", input_dir))
+    p.wait()
+    os.chdir("/app")
 
     print("Running scene2pset")
     input_dir = "/app/pipe_out/matches/MVE"
